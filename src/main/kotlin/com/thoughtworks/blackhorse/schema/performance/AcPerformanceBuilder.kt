4d9bd6e3736906b78d116b122bb915cef0ad4694
Andong Liao <adliao@thoughtworks.com>
2021-06-29 16:50:30 +0800

    extract function of find flow by id in list

--- src/main/kotlin/com/thoughtworks/blackhorse/schema/performance/AcPerformanceBuilder.kt
+++ src/main/kotlin/com/thoughtworks/blackhorse/schema/performance/AcPerformanceBuilder.kt
@@ -2,6 +2,7 @@ package com.thoughtworks.blackhorse.schema.performance
 
 import com.thoughtworks.blackhorse.schema.story.AcceptanceCriteria
 import com.thoughtworks.blackhorse.schema.story.Flow
+import com.thoughtworks.blackhorse.utils.findByKeyOrNext
 
 class AcPerformanceBuilder(
     private val ac: AcceptanceCriteria,
@@ -9,19 +10,8 @@ class AcPerformanceBuilder(
     private val flows = mutableListOf<FlowPerformanceBuilder>()
 
     fun flow(id: String? = null, fn: FlowPerformanceBuilder.() -> Unit) {
-        val flow = id?.let(this::findFlowById) ?: findNextFlow()
+        val flow = ac.flows.findByKeyOrNext(Flow::id, id, flows.size)
         flows.add(FlowPerformanceBuilder(flow).apply(fn))
-    }
-
-    private fun findFlowById(id: String): Flow {
-        return ac.flows.find { it.id == id }
-            ?: throw IllegalArgumentException("No such flow with id: $id")
-    }
-
-    private fun findNextFlow(): Flow {
-        if (flows.size == ac.flows.size)
-            throw IllegalArgumentException("You have complete all flows, don't add invalid flow")
-        return ac.flows[flows.size]
     }
 
     fun build() = flows.flatMap(FlowPerformanceBuilder::build)
